a4040328c20377e14380d28e0fa293e5286afba9
DevBGarcia <DevBGarcia@gmail.com>
2021-05-16 14:26:08 -0700

    Updates

---
+++ precache-manifest.75bdc86cd38c04d08d485689fa30c3e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0998ed401297d58fd4af34068de74308",
+    "url": "/gif-search-react-app/index.html"
+  },
+  {
+    "revision": "2b0ad238d34f0cea6d29",
+    "url": "/gif-search-react-app/static/css/main.7a0e6566.chunk.css"
+  },
+  {
+    "revision": "5d985b301d5ae01a4bbb",
+    "url": "/gif-search-react-app/static/js/2.ed5ecfce.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gif-search-react-app/static/js/2.ed5ecfce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b0ad238d34f0cea6d29",
+    "url": "/gif-search-react-app/static/js/main.bffb729d.chunk.js"
+  },
+  {
+    "revision": "5e0309792f12c11ebf7a",
+    "url": "/gif-search-react-app/static/js/runtime-main.c69bcb8c.js"
+  }
+]);
